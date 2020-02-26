a86af464baeefa675a7ed441651d753500c777c5
0ldf1re <agni.purani@gmail.com>
2020-02-27 00:04:32 +0530

    intial gmae making attempt

--- FaceTrackGame.py
+++ FaceTrackGame.py
@@ -1,5 +1,7 @@
 import cv2
 import numpy as np
+from random import seed
+from random import random
 
 face_cascade = cv2.CascadeClassifier('//usr//share//opencv//haarcascades//haarcascade_frontalface_default.xml')
 cap = cv2.VideoCapture(0)
@@ -21,8 +23,14 @@ def FindAFace():
             print("Detected!")
             break
 
-
+speed = 4
 FindAFace()
+seed(1)
+rectYi = [0, 0, 0 ,0]
+rectYf =  [0, 0, 0, 0]
+safeZone = [0, 0, 0, 0]
+safeZoneFlag = [True, False, True, True]
+rectFlag = [True, False, False, False]
 
 while(True):
     _, frame = cap.read()
@@ -44,7 +52,50 @@ while(True):
     cv2.rectangle(frame, loc, (loc[0] + w, loc[1] + h), (0,255,255), 2) 
     roi = frame[loc[1]:loc[1]+h, loc[0]:loc[0]+w]
 
+    # The game code begins from here. The above code was only for detection and tracking
+    game = np.zeros(frame.shape, dtype=np.uint8)
+    rectCount = 0
+
+    centerX = int(loc[0] + w / 2)
+    centerY = int(loc[1] + h / 2)
+
+    for i in range(4):
+        if safeZoneFlag[i]:
+            safeZone[i] = int(frame.shape[1] * 15 / 16 * random() + 20)
+            safeZoneFlag[i] = False
+
+        if rectFlag[i]:
+            cv2.rectangle(game, (0, rectYi[i]), (safeZone[i] - 60, rectYf[i]), (128, 128, 0), -1)
+            cv2.rectangle(game, (safeZone[i] + 60, rectYi[i]), (frame.shape[1] - 1, rectYf[i]), (128, 128, 0), -1)
+
+            if rectYf[i] >= 70:
+                rectYi[i] += speed
+
+            rectYf[i] += speed
+
+            if rectYf[i] >= frame.shape[0]:
+                rectYf[i] = frame.shape[0] - 1
+
+            if rectYi[i] >= frame.shape[0]:
+                rectYi[i] = 0
+                rectYf[i] = 0
+                rectFlag[i] = 0
+                safeZoneFlag = 0
+
+            if rectYi[i] >= 90:
+                if i != 3:
+                    rectFlag[i + 1] = True
+                    safeZoneFlag[i + 1] = True
+                else:
+                    rectFlag[0] = True
+                    safeZoneFlag[0] = True
+
+    print(rectYf, rectYi)
+
+    cv2.circle(game, (centerX, centerY), 10, (0, 140, 255), 11)
+
     cv2.imshow('tracker', frame)
+    cv2.imshow('Game', game)
     if cv2.waitKey(1) & 0xFF == 27 :
         break
 
